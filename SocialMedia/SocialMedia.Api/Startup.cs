7274da1237a39e7662e2e6c12a40dec41f897099
jjaramilloa <jjaramillo01@hotmail.com>
2020-06-11 19:45:36 -0500

    Video 9_ Patron Repositorio.

--- SocialMedia/SocialMedia.Api/Startup.cs
+++ SocialMedia/SocialMedia.Api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SocialMedia.Core.Interfaces;
+using SocialMedia.Core.Services;
 using SocialMedia.Infrastructure.Data;
 using SocialMedia.Infrastructure.Filters;
 using SocialMedia.Infrastructure.Repositories;
@@ -42,7 +43,12 @@ namespace SocialMedia.Api
             services.AddDbContext<SocialMediaContext>(Option => 
             Option.UseSqlServer(Configuration.GetConnectionString("SocialMedia")));
 
+
+            services.AddTransient<IPostService, PostService>();
             services.AddTransient<IPostRepository, PostRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+
+
 
             //registramos un filto de forma global.
             services.AddMvc(options =>
