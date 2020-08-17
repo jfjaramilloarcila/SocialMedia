51067a83567aef3a63cd2f0e6c5147aaaba0f249
jjaramilloa <jjaramillo01@hotmail.com>
2020-08-17 16:43:58 -0500

    Video_16 token parte 1

---
+++ SocialMedia/SocialMedia.Api/Controllers/TokenController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SocialMedia.Core.Entities;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Authenticaion(UserLogin login)
+        {
+            //if it is a valid user
+            if (IsValidUser(login))
+            {
+                var token = GenerateToken();
+                return Ok(new { token });
+            }
+            return NotFound();
+        }
+
+        private bool IsValidUser(UserLogin login)
+        {
+            return true;
+        }
+
+        private string GenerateToken()
+        {
+            //Header
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Authentication:SecretKey"]));
+            var singningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+            var header = new JwtHeader(singningCredentials);
+
+            //Claims Info que se quiere agregar en el cuerpo del mensaje  para poder crear paload
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, "Juan Felipe"),
+                new Claim(ClaimTypes.Email, "jjaramillo01@hotmail.com"),
+                new Claim(ClaimTypes.Role, "Administrador"),
+            };
+
+            //Payload
+            var payload = new JwtPayload
+            (
+                _configuration["Authentication:Issuer"],
+                 _configuration["Authentication:Audience"],
+                 claims,
+                 DateTime.Now,
+                 DateTime.UtcNow.AddMinutes(2)
+            );
+
+            //Token
+            var token = new JwtSecurityToken(header, payload);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- SocialMedia/SocialMedia.Api/Startup.cs
+++ SocialMedia/SocialMedia.Api/Startup.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -8,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using SocialMedia.Core.CustomEntities;
 using SocialMedia.Core.Interfaces;
@@ -20,6 +22,7 @@ using SocialMedia.Infrastructure.Services;
 using System;
 using System.IO;
 using System.Reflection;
+using System.Text;
 
 namespace SocialMedia.Api
 {
@@ -85,6 +88,28 @@ namespace SocialMedia.Api
                 doc.IncludeXmlComments(xmlPath);
             });
 
+            //Para esquema de authenticacion por token
+            services.AddAuthentication(optins =>
+            {
+                optins.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                optins.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(Options =>
+            {
+                Options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Authentication:Issuer"],
+                    ValidAudience = Configuration["Authentication:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Authentication:SecretKey"]))
+                };
+
+            });
+
+
+
             //registramos un filto de forma global.
             services.AddMvc(options =>
             {
@@ -116,7 +141,9 @@ namespace SocialMedia.Api
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseAuthorization();          
 
             app.UseEndpoints(endpoints =>
             {
